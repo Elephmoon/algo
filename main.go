e7aee6704ecdc43ce9f7d1ed2a64cc130de24080
Alexandr Kizilow <alexandr.kizilow@gmail.com>
2020-01-23 22:21:25 +0300

    add binarySearch and StupidSearch

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"algo/algorithms"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	arrSize := 100000000
+	list := make([]int, arrSize)
+	elem := rand.Intn(arrSize)
+
+	for i := 0; i < arrSize; i++ {
+		list[i] = rand.Intn(arrSize)
+	}
+
+	startBinary := time.Now()
+	sortedPosition, found := algorithms.BinarySearchInt(list, elem)
+	fmt.Println("Binary search time = ", time.Since(startBinary))
+	if found {
+		fmt.Println(sortedPosition)
+	} else {
+		fmt.Println("Not found ", elem)
+	}
+
+	startStupid := time.Now()
+	sortedPosition, found = algorithms.StupidSearchInt(list, elem)
+	fmt.Println("Stupid search time = ", time.Since(startStupid))
+	if found {
+		fmt.Println(sortedPosition)
+	} else {
+		fmt.Println("Not found", elem)
+	}
+}
