aec3acb599886ab169aa510a2e1f94b055cd4690
Elephmoon <alexandr.kizilow@gmail.com>
2020-07-22 20:01:36 +0300

    implement quickSort

--- algorithms/quick_sort.go
+++ algorithms/quick_sort.go
@@ -1,6 +1,23 @@
 package algorithms
 
+import "math/rand"
+
 func QuickSort(data []int32) []int32 {
-	var result []int32
-	return result
+	if len(data) < 2 {
+		return data
+	}
+	left := 0
+	right := len(data) - 1
+	pivot := rand.Int() % len(data)
+	data[pivot], data[right] = data[right], data[pivot]
+	for i := range data {
+		if data[i] < data[right] {
+			data[left], data[i] = data[i], data[left]
+			left++
+		}
+	}
+	data[left], data[right] = data[right], data[left]
+	QuickSort(data[:left])
+	QuickSort(data[left+1:])
+	return data
 }
