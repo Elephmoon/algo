6a2e33ebd796a7dd6e658b42d98a5ec9bd708668
Elephmoon <alexandr.kizilow@gmail.com>
2020-10-07 21:22:20 +0300

    implement binaryTree with Insert

---
+++ algorithms/bin_tree.go
@@ -0,0 +1,35 @@
+package algorithms
+
+import "errors"
+
+var (
+	errNilTree = errors.New("tree is nill")
+)
+
+type Node struct {
+	value int
+	left  *Node
+	right *Node
+}
+
+func (n *Node) Insert(value int) error {
+	if n == nil {
+		return errNilTree
+	}
+	if n.value > value {
+		if n.left == nil {
+			n.left = &Node{value: value}
+			return nil
+		}
+		return n.left.Insert(value)
+	}
+
+	if n.value < value {
+		if n.right == nil {
+			n.right = &Node{value: value}
+		}
+		return n.right.Insert(value)
+	}
+
+	return nil
+}
